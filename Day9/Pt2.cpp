f97e35a0a5cc2d4ae2c22931995910924a2043a1
dmforeacre <dmforeacre@gmail.com>
2024-12-10 22:06:14 -0500

    Still working

--- Day9/Pt2.cpp
+++ Day9/Pt2.cpp
@@ -8,15 +8,13 @@
 
 struct Block
 {
+    int value;
     int size;
-    int index;
-    bool free;
 
-    Block(int s, int i, bool f)
+    Block(int v, int s)
     {
+        value = v;
         size = s;
-        index = i;
-        free = f;
     }
 };
 
@@ -25,7 +23,7 @@ int main()
     std::string text;
     std::fstream inFile("test.txt");
 
-    std::vector<Block> blocks;
+    std::vector<Block> usedBlocks, freeBlocks;
 
     Timer t;
     t.startTimer();
@@ -33,46 +31,37 @@ int main()
     long total = 0;
     getline(inFile, text);
 
+    int diskSize = 0;
     for(int i = 0; i < text.size(); ++i)
     {
-        blocks.push_back(Block(text[i]-48, i, i%2 == 0));
+        diskSize += text[i] - 48;
+        if(i%2 == 0)
+            usedBlocks.push_back(Block(i / 2, text[i] - 48));
+        else
+            freeBlocks.push_back(Block(i / 2, text[i] - 48));
     }
 
     inFile.close();
 
-    std::list<Block> reversedBlocks;
-    int diskSize = 0;
     std::vector<int> disk = std::vector<int>(diskSize);
-    int index = 0;
-    for(Block b : blocks)
-    {
-        for(int i = 0; i < b.size; ++i)
-        {
-            disk[index] = 
-        }
-        diskSize += b.size;
-        if(!b.free)
-        {
-            reversedBlocks.push_front(b);
-        }
-    }
 
-    for(Block b : reversedBlocks)
+    int i = 0;
+    int index = 0;
+    while(i < diskSize)
     {
-        Block lastBlock = usedBlocks.back();
-        usedBlocks.pop_back();
-        int i = 0;
-        do
+        for(int j = 0; j < usedBlocks[index].size; ++j)
+        {
+            disk[i] = usedBlocks[index].value;
+            ++i;
+        }
+        for(int j = 0; j < freeBlocks[index].size; ++j)
         {
             ++i;
-        } while (freeBlocks[i].size < lastBlock.size && i < freeBlocks.size());
-        if(i < freeBlocks.size())
-        {
-            for(int j = )
         }
-        
     }
     
+    printVector(disk);
+
     t.endTimer();
 
     msg(total,"      in", t.getElapsed(),"ms");
