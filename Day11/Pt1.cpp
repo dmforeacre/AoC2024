dee09e223c5adb2dac20d1512d39efdc0697a25d
dmforeacre <dmforeacre@gmail.com>
2024-12-12 12:52:35 -0500

    Finished D11P1

---
+++ Day11/Pt1.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <numeric>
+#include <list>
+#include "../Utils.h"
+
+int main()
+{
+    std::string text;
+    std::fstream inFile("input.txt");
+
+    Timer t;
+    t.startTimer();
+
+    int total = 0;
+
+    getline(inFile, text);
+
+    std::vector<std::string> split = splitString(text);
+    std::list<long> stones;
+    for(std::string s : split)
+        stones.push_back(std::stol(s));
+
+    for(int i = 0; i < 25; ++i)
+    {
+        for(auto it = stones.begin(); it != stones.end(); ++it)
+        {
+            std::string str = std::to_string(*it);
+            if(*it == 0)
+                *it = 1;
+            else if(str.size() % 2 == 0)
+            {
+                stones.insert(it, std::stol(str.substr(0, str.length() / 2)));
+                *it = std::stol(str.substr(str.length() / 2, str.length() / 2));
+            }
+            else
+            {
+                *it *= 2024;
+            }
+        }
+
+    }
+
+    total = stones.size();
+    
+    t.endTimer();
+
+    msg(total,"      in", t.getElapsed(),"ms");
+    return 0;
+}
---
+++ Day11/Pt2.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <numeric>
+#include <list>
+#include "../Utils.h"
+
+void traverse(std::vector<std::vector<int>>& grid, int& total, Point p)
+{
+    if(p.at(grid) == 9)
+    {
+        ++total;
+        return;
+    }
+
+    Point up = Point(p.x, p.y-1);
+    Point down = Point(p.x, p.y+1);
+    Point left = Point(p.x-1, p.y);
+    Point right = Point(p.x+1, p.y);
+    if(up.isValid(grid) && up.at(grid) == p.at(grid) + 1)
+        traverse(grid, total, up);
+    if(down.isValid(grid) && down.at(grid) == p.at(grid) + 1)
+        traverse(grid, total, down);
+    if(left.isValid(grid) && left.at(grid) == p.at(grid) + 1)
+        traverse(grid, total, left);
+    if(right.isValid(grid) && right.at(grid) == p.at(grid) + 1)
+        traverse(grid, total, right);
+}
+
+int main()
+{
+    std::string text;
+    std::fstream inFile("input.txt");
+
+    Timer t;
+    t.startTimer();
+
+    int total = 0;
+    std::vector<std::vector<int>> grid;
+    std::vector<Point> trailheads;
+
+    int row = 0;
+    while(!inFile.eof())
+    {
+        getline(inFile, text);
+        std::vector<int> line;
+        int col = 0;
+        for(int i = 0; i < text.size(); ++i)
+        {
+            if(text[i] - 48 == 0)
+                trailheads.push_back(Point(col, row));
+            line.push_back(text[i] - 48);
+            ++col;
+        }
+        grid.push_back(line);
+        ++row;
+    }
+
+    inFile.close();
+
+    for(Point p : trailheads)
+        traverse(grid, total, p);
+    
+    t.endTimer();
+
+    msg(total,"      in", t.getElapsed(),"ms");
+    return 0;
+}
