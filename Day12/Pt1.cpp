6a2efcb114b64f9b0a5fa80fe1277f1e5b1326d9
dmforeacre <dmforeacre@gmail.com>
2024-12-13 21:02:46 -0500

    Finished D12p1

--- Day12/Pt1.cpp
+++ Day12/Pt1.cpp
@@ -6,67 +6,51 @@
 #include <list>
 #include "../Utils.h"
 
-void expand(std::vector<std::vector<char>>& grid, std::vector<std::vector<char>>& visited, Point p, std::pair<int, int>& values)
+bool find(std::vector<Point>& ptList, Point p)
 {
-    visited[p.y][p.x] = -1;
+    for(Point pt : ptList)
+        if(pt == p)
+            return true;
+    return false;
+}
+
+void expand(std::vector<std::vector<char>>& grid, std::vector<Point>& visited, Point p, std::pair<int, int>& values)
+{
+    if(!find(visited, p))
+        visited.push_back(p);
+    else
+        return;
+    ++values.first;
     Point up = Point(p.x,p.y-1);
     Point down = Point(p.x,p.y+1);
     Point left = Point(p.x-1,p.y);
     Point right = Point(p.x+1,p.y);
-    if(up.isValid(grid) && up.at(grid) != p.at(grid))
-    {
-        ++values.first;
-        ++values.second;
-        //msg("   Up different");
-    }else if(!up.isValid(grid))
+    if(!up.isValid(grid) || (up.isValid(grid) && up.at(grid) != p.at(grid)))
     {
         ++values.second;
-        //msg("   Up edge");
-    }else if(up.at(grid) == p.at(grid))
+    }else if(up.at(grid) == p.at(grid) && !find(visited, up))
     {
-        ++values.first;
         expand(grid, visited, up, values);
     }
-    if(down.isValid(grid) && down.at(grid) != p.at(grid))
-    {
-        ++values.first;
-        ++values.second;
-        //msg("   Down different");
-    }else if(!down.isValid(grid))
+    if(!down.isValid(grid) || (down.isValid(grid) && down.at(grid) != p.at(grid)))
     {
         ++values.second;
-        //msg("   Down edge");
-    }else if(down.at(grid) == p.at(grid))
+    }else if(down.at(grid) == p.at(grid) && !find(visited, down))
     {
-        ++values.first;
         expand(grid, visited, down, values);
     }
-    if(left.isValid(grid) && left.at(grid) != p.at(grid))
-    {
-        ++values.first;
-        ++values.second;
-        //msg("   Left different");
-    }else if(!left.isValid(grid))
+    if(!left.isValid(grid) || (left.isValid(grid) && left.at(grid) != p.at(grid)))
     {
         ++values.second;
-        //msg("   Left edge");
-    }else if(left.at(grid) == p.at(grid))
+    }else if(left.at(grid) == p.at(grid) && !find(visited, left))
     {
-        ++values.first;
         expand(grid, visited, left, values);
     }
-    if(right.isValid(grid) && right.at(grid) != p.at(grid))
-    {
-        ++values.first;
-        ++values.second;
-        //msg("   Right different");
-    }else if(!right.isValid(grid))
+    if(!right.isValid(grid) || (right.isValid(grid) && right.at(grid) != p.at(grid)))
     {
         ++values.second;
-        //msg("   Right edge");
-    }else if(right.at(grid) == p.at(grid))
+    }else if(right.at(grid) == p.at(grid) && !find(visited, right))
     {
-        ++values.first;
         expand(grid, visited, right, values);
     }
 }
@@ -74,7 +58,7 @@ void expand(std::vector<std::vector<char>>& grid, std::vector<std::vector<char>>
 int main()
 {
     std::string text;
-    std::fstream inFile("test.txt");
+    std::fstream inFile("input.txt");
 
     Timer t;
     t.startTimer();
@@ -82,7 +66,6 @@ int main()
     int total = 0;
 
     std::vector<std::vector<char>> grid;
-    std::vector<std::vector<char>> visited;
     // each plot pair is Area, Perimeter
     std::list<std::pair<int, int>> plots;
     while(!inFile.eof())
@@ -96,11 +79,13 @@ int main()
         grid.push_back(line);
     }
 
+    std::vector<Point> visited;
+
     for(int i = 0; i < grid.size(); ++i)
     {
         for(int j = 0; j < grid[i].size(); ++j)
         {
-            if(visited[i][j] != '.')
+            if(!find(visited, Point(j, i)))
             {
                 std::pair<int, int> plot;
                 expand(grid, visited, Point(j, i), plot);
