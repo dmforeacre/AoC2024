21a14c7efdba47427f84782a10fefe5fbc6c73c8
dmforeacre <dmforeacre@gmail.com>
2024-12-16 18:36:45 -0500

    Added memoizer code

--- Day11/Pt2.cpp
+++ Day11/Pt2.cpp
@@ -1,93 +1,69 @@
 #include <iostream>
 #include <fstream>
-#include <string>
-#include <vector>
-#include <numeric>
-#include <list>
-#include <unordered_map>
+#include <functional>
 #include <cmath>
 #include "../Utils.h"
+#include "../memoizer.h"
 
-std::list<long> fullyExpand(long num)
+std::vector<int> evolveStone(int stone)
 {
-    std::list<long> stones;
-    int strSize = std::to_string(num).size();
-    int power = pow(10, strSize / 2); 
-    if(num == 0)
+    std::vector<int> newStones;
+    int strSize = std::to_string(stone).size();
+    int power = pow(10, strSize / 2);  
+    if(stone == 0)
+        newStones.push_back(1);
+    else if (strSize % 2 == 0)
     {
-        //msg("               Returning to 1");
-        stones.push_front(1);
-        return stones;
-    }
-    else if(strSize % 2 == 0)
-    {
-        int first = std::stol(num) / power;
-        int second = std::stol(num) % power;
-        //msg("               Splitting stone into",first,second);
-        return std::to_string(first) + " " + std::to_string(second);
-    }
-    else
-    {
-        //msg("               Multiply by 2024:", std::stoi(num) * 2024);
-        return std::to_string(std::stol(num) * 2024);
-    }
+        newStones.push_back(stone / power);
+        newStones.push_back(stone % power);        
+    }else
+        newStones.push_back(stone * 2024);
+    return newStones;
 }
 
-void blink(std::list<long>& stones, std::unordered_map<long, std::list<long>>& memo)
+std::vector<int> blink(std::vector<int> stones)
 {
-    //msg("           Blinking",num);
-    
-    
+    std::vector<int> newStones;
+    for(int s : stones)
+    {
+        std::vector<int> results = invoke_memoized(evolveStone, [](int stone) -> std::vector<int>{    std::vector<int> newStones;
+    int strSize = std::to_string(stone).size();
+    int power = pow(10, strSize / 2);  
+    if(stone == 0)
+        newStones.push_back(1);
+    else if (strSize % 2 == 0)
+    {
+        newStones.push_back(stone / power);
+        newStones.push_back(stone % power);        
+    }else
+        newStones.push_back(stone * 2024);
+    return newStones;});
+        for(int i : results)
+            newStones.push_back(i);
+    }
+    return newStones;
 }
 
 int main()
 {
     std::string text;
-    std::fstream inFile("input.txt");
+    std::fstream inFile("test.txt");
 
     Timer t;
     t.startTimer();
 
     long total = 0;
+    std::vector<int> stones;
 
     getline(inFile, text);
 
-    std::vector<std::string> split = splitString(text);
-    std::list<std::string> stones;
-    for(std::string s : split)
-        stones.push_back(s);
-    std::cout << std::endl;
-    for(int i = 0; i < 75; ++i)
-    {
-        //msg("Round",i);
-        std::string stonesString = "";
-        for(std::string s : stones)
-        {   
-            //msg("   Checking",s);
-            std::string results;
-            if(memo.find(s) == memo.end())
-            {
-                results = blink(s);
-                memo[s] = results;
-                //msg("       Creating memo ->", s,"with results",results);
-            }
-            else
-            {
-                results = memo[s];
-                //msg("       Found memo ->",s,"with results",results);
-            }
-            stonesString = results + " " + stonesString;       
-        }
-        //msg("Current line:",stonesString);
-        stones.clear();
-        for(std::string newS : splitString(stonesString))
-        {
-            if(newS != "")
-                stones.push_front(newS);
-        }
-        
-    }
+    for(std::string s : splitString(text))
+        stones.push_back(std::stoi(s));
 
+    for(int i = 0; i < 25; ++i)
+        //stones = invoke_memoized(blink, stones);
+        stones = blink(stones);
+    
     total = stones.size();
     
     t.endTimer();
